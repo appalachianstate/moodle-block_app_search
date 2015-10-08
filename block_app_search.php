bb78277e3c0bf667db6987ba042e0d2f2b992db6
Michelle Melton <meltonml@appstate.edu>
2015-10-08 09:14:15 -0400

    Initial commit

---
+++ block_app_search.php
@@ -0,0 +1,62 @@
+<?php
+
+  // This file is part of Moodle - http://moodle.org/
+  //
+  // Moodle is free software: you can redistribute it and/or modify
+  // it under the terms of the GNU General Public License as published by
+  // the Free Software Foundation, either version 3 of the License, or
+  // (at your option) any later version.
+  //
+  // Moodle is distributed in the hope that it will be useful,
+  // but WITHOUT ANY WARRANTY; without even the implied warranty of
+  // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  // GNU General Public License for more details.
+  //
+  // You should have received a copy of the GNU General Public License
+  // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+  /**
+   * AppSearch block plugin
+   *
+   * @package    block_app_search
+   * @author     Michelle Melton 
+   * @copyright  2015 Appalachian State University
+   * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+   */
+
+  class block_app_search extends block_base 
+  {
+      function init() 
+      {
+          $this->title = get_string('pluginname', 'block_app_search');
+          $this->blockConfigs = get_config('block_app_search');
+      }
+
+      function get_content() 
+      {
+          global $CFG, $USER;
+
+          if($this->content !== NULL) {
+              return $this->content;
+          }
+        
+          $this->content = new stdClass;
+          $this->content->footer = '';
+          
+          $html = '';
+          if (!empty($CFG->block_app_search_html)) {
+            $html= $CFG->block_app_search_html;
+          }
+          $this->content->text = $html; 
+          return $this->content;
+      }
+      
+      /**
+       * Override
+       * @see block_base::has_config()
+       */
+      function has_config()
+      {
+          return true;
+      }
+  }
---
+++ lang/en/block_app_search.php
@@ -0,0 +1,34 @@
+<?php
+
+  // This file is part of Moodle - http://moodle.org/
+  //
+  // Moodle is free software: you can redistribute it and/or modify
+  // it under the terms of the GNU General Public License as published by
+  // the Free Software Foundation, either version 3 of the License, or
+  // (at your option) any later version.
+  //
+  // Moodle is distributed in the hope that it will be useful,
+  // but WITHOUT ANY WARRANTY; without even the implied warranty of
+  // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  // GNU General Public License for more details.
+  //
+  // You should have received a copy of the GNU General Public License
+  // along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+  /**
+   * AppSearch block plugin
+   *
+   * @package    block_app_search
+   * @author     Michelle Melton 
+   * @copyright  2015 Appalachian State University
+   * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+   */
+
+  $string['pluginname'] = 'AppSearch';
+  
+  $string['app_search'] = 'AppSearch';
+  $string['app_search:addinstance'] = 'Add a new AppSearch block';
+  $string['app_search:myaddinstance'] = 'Add a new AppSearch block to the My Moodle page';
+  $string['app_search_settings'] = 'AppSearch settings';
+  $string['app_search_hrml'] = 'HTML';
+  $string['app_search_html_desc'] = 'Enter the HTML for the AppSearch block.';
